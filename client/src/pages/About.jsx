78a58e045101766e18f2d2047e20e3b36b6374d6
RajkumarS2004 <rajkumarsraj12@gmail.com>
2025-05-28 12:09:15 +0530

    about-page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">About Us</h1>
+            <p className="text-lg text-gray-700 max-w-md text-center">
+            We are a team of passionate developers dedicated to building amazing web applications.
+            Our mission is to create user-friendly and efficient software solutions that make a difference.
+            </p>
+        </div>
+    </>
+  )
+}
