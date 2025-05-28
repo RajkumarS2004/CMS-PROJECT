64993b7d753cc76cbf5dc9634f6c7de914983c40
RajkumarS2004 <rajkumarsraj12@gmail.com>
2025-05-28 14:34:33 +0530

    layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import About from './pages/About'
-
+import Basiclayouts from './layout/Basiclayouts'
 export default function App() {
   return (
     <>
+    <Basiclayouts/>
     <About/>
     </>
   )
